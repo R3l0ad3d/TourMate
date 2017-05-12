4768139ed8ce68b43d532e1a27a940c7ab53c58e
r3l0ad3d <azizur.rahman.bu@gmail.com>
2017-05-13 03:38:24 +0600

    model class update

--- app/src/main/java/com/example/r3l0ad3d/tourmate/Fragments/MomentGalleryFragment.java
+++ app/src/main/java/com/example/r3l0ad3d/tourmate/Fragments/MomentGalleryFragment.java
@@ -1,6 +1,7 @@
 package com.example.r3l0ad3d.tourmate.Fragments;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.r3l0ad3d.tourmate.R;
+import com.example.r3l0ad3d.tourmate.databinding.FragmentMomentGalleryBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MomentGalleryFragment extends Fragment {
+
+    private FragmentMomentGalleryBinding binding;
 
     public MomentGalleryFragment() {
         // Required empty public constructor
@@ -23,7 +27,10 @@ public class MomentGalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_moment_gallery, container, false);
+        View view = inflater.inflate(R.layout.fragment_moment_gallery, container, false);
+        binding = DataBindingUtil.bind(view);
+
+        return view;
     }
 
 }
